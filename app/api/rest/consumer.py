7bf8c967046da605b384c9dd0a37c2342b49cd37
Ragini Verma <ragini.vermaa@gmail.com>
2018-07-11 18:06:07 +0800

    returns resized numpy array

--- app/api/rest/consumer.py
+++ app/api/rest/consumer.py
@@ -1,9 +1,17 @@
-import requests
 from app.api.rest.test import Receive
 from app.api.rest.product import get_info
+import numpy
+from PIL import Image
+import io
 
 def Consume(uploadedImage):
+
     image_uploaded = uploadedImage.read()
-    r = Receive(image_uploaded)
-    #print(r)
+    image_data = image_uploaded
+    rgb = Image.open(io.BytesIO(image_data)).convert("RGB")
+    #print ("unresized: ", (numpy.array(rgb)).shape)
+    resized = rgb.resize((500,500), Image.ANTIALIAS)
+    numpy_array = numpy.array(resized)
+    #print("resized: ", numpy_array.shape)
+    r = Receive(numpy_array)
     return get_info(r)
