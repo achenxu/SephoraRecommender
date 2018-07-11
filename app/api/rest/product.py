d6e45f8ddaa8a5b5ddab2c3e899fb8a222634643
Ragini Verma <ragini.vermaa@gmail.com>
2018-07-11 16:57:34 +0800

    error handling needed

--- app/api/rest/product.py
+++ app/api/rest/product.py
@@ -1,12 +1,12 @@
 from flask import request
 from flask_restplus import Api
 import json
-
+from operator import itemgetter
+import requests
 from app.api.rest.base import BaseResource, SecureResource
 from app.api import api_rest
 
-
-def @get_info(listOfMatches):
+def get_info(listOfMatches):
     # sort the list of dictionaries by confidence in descending order
     # take only the first 10 products
     sortedList = sorted(listOfMatches, key=itemgetter('confidence'), reverse=True)[:10]
@@ -15,15 +15,16 @@ def @get_info(listOfMatches):
 
     # for each of the 10 products, call the sephora endpoint to get product name, brand, price, variant image, variant name
     for product in sortedList:
-                r = request.get('https://luxola-staging-api.herokuapp.com/api/v2/products/' + item.get('productId') + '?include=variants').json()
-                listOfIncluded = r.data.included
+                r = requests.get('https://sephora.sg/api/v2/products/' + product.get('productId') + '?include=variants', headers={'Accept-Language':'en-SG', 'Content-Type':'application/json'}).json()
+                print(r.keys())
+                listOfIncluded = r["included"]
                 #retrieve the variant dictionary
-                variant = next((v for v in listOfIncluded if v['id'] == item.get('variantId')), None)
-                variantName = variant.attributes.name
-                variantPrice = variant.attributes.price
-                variantImage = variant.attributes.image-url
-                productName = variant.attributes.product-name
-                brandName = variant.attributes.brand-name
+                variant = next((v for v in listOfIncluded if v['id'] == product.get('variantId')), None)
+                variantName = variant['attributes']['name']
+                variantPrice = variant['attributes']['price']
+                variantImage = variant['attributes']['image-url']
+                productName = variant['attributes']['product-name']
+                brandName = variant['attributes']['brand-name']
 
                 productInfo = {"variantName": variantName, "variantPrice": variantPrice, "variantImage": variantImage, "productName": productName, "brandName": brandName}
                 listOfProductInfoDictionaries.append(productInfo)
